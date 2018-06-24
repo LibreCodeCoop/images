b5d547e6245f5754d293cce38d7c594b92cf63c6
William Correa <wilcorrea@gmail.com>
2018-06-24 17:29:13 -0300

    Organizar imagens

--- quasar/build.sh
+++ quasar/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -t lyseontech/quasar .
+DIR_NAME=$(dirname $(readlink -f ${0}))
+
+docker build -t lyseontech/quasar ${DIR_NAME}/${1}
 docker tag lyseontech/quasar lyseontech/quasar:v${1}
 docker push lyseontech/quasar:v${1}
 docker push lyseontech/quasar:latest
